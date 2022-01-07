d47a512603837b5ec2870df8fd55244abbe3f8c2
Austin Fulford <61794234+AustinFulford@users.noreply.github.com>
2022-01-07 16:02:08 -0500

    Add files via upload

---
+++ Project Hogwarts/Controllers/DAL/SchoolConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using Hogwarts.DAL;
+using System.Data.Entity.Infrastructure.Interception;
+
+namespace Hogwarts.DAL
+{
+    public class SchoolConfiguration : DbConfiguration
+    {
+        public SchoolConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
+        }
+    }
+}
